2664509c4dfb909040cf8109fbdcf432035d3d83
Abe Chen <abechen@hyenatek.com>
2022-02-24 17:27:14 +0800

    working on navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,70 @@
-import { Box, Container } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { Box, Container, Flex, Stack, Heading, Link, useColorModeValue } from '@chakra-ui/react';
+
+const LinkItem = ({ href, path, target, children, ...props }) => {
+  const active = path === href;
+  const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
+  return (
+    <NextLink href={href} passHref scroll={false}>
+      <Link
+        p={2}
+        bg={active ? 'grassTeal' : undefined}
+        color={active ? '#202023' : inactiveColor}
+        target={target}
+        {...props}
+      >
+        {children}
+      </Link>
+    </NextLink>
+  );
+};
+
 const Navbar = ({ path }) => {
-	console.log('path', path);
-	return (
-		<Box as="nav" position="fix" w="100%">
-			<Container
-				maxW="container.md"
-				display="flex"
-				align="center"
-				justify="space-around"
-			>
-				<Box>Logo</Box>
-				<Box>ssss</Box>
-			</Container>
-		</Box>
-	);
+  console.log('path', path);
+  return (
+    <Box
+      as="nav"
+      position="fixed"
+      w="100%"
+      css={{ backdropFilter: 'blur(10px)' }}
+      zIndex={1}
+    >
+      <Container
+        maxW="container.md"
+        p={2}
+        display="flex"
+        wrap="wrap"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Flex align="center" mr={5}>
+          <Heading as="h1" size="lg" letterSpacing={'tighter'}>
+            LOGO
+          </Heading>
+        </Flex>
+        {/* <Stack
+          flexDirection={{ base: 'columm', md: 'row' }}
+          display={{ base: 'none', md: 'flex' }}
+          width={{ base: 'full', md: 'auto' }}
+          justifyContent="center"
+          alignItems="center"
+          flexGrow={1}
+          mt={{ base: 4, md: 0 }}
+        > */}
+          <LinkItem href="/about" path={path}>
+            about
+          </LinkItem>
+          <LinkItem href="/works" path={path}>
+            works
+          </LinkItem>
+          <LinkItem href="https://github.com/abe1272001/abe.com/tree/main">
+            github
+          </LinkItem>
+        {/* </Stack> */}
+        <Box>mode</Box>
+      </Container>
+    </Box>
+  );
 };
 
 export default Navbar;
--- components/layouts/main.js
+++ components/layouts/main.js
@@ -6,7 +6,7 @@ import Footer from '../Footer';
 const Main = ({ children, router }) => {
 	console.log('router', router);
 	return (
-		<Box as="main">
+		<Box as="main" pb={8}>
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta name="description" content="Abe's homepage" />
